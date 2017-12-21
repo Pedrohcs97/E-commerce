c2003c0b323713356658d45e6c4e27594956071e
Unknown <pedrocamargos.phcs@gmail.com>
2017-12-21 19:59:18 -0200

    Fim

--- web_back/src/main/java/com/pedro/web/domain/produto/ProdutoRepository.java
+++ web_back/src/main/java/com/pedro/web/domain/produto/ProdutoRepository.java
@@ -11,12 +11,4 @@ import org.springframework.stereotype.Repository;
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     public List<Produto> findByNome(String nome);
-    
-   /*@Query("SELECT p FROM Produto p WHERE p.nome = :nome")
-    public boolean verificarNomeExistente(@Param("nome") String nome);
-
-    public Produto findByNome(String nome);
-
-    /*@Query("SELECT p FROM Produto p WHERE p.categoria = :categoria")
-    public List<Produto> buscarProdutoPelaCategoria(@Param("categoria") Categoria categoria);*/
 }
